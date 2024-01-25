432437dda2af5f944fcf431228c631cfffe0c8b6
aintitfun <opolofdez@gmail.com>
2024-01-25 13:19:39 +0100

    more ignored items, corrected the routing to logout now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserTimeComponent } from './user-time/user-time.component';
 import { TimeWindowComponent } from './time-window/time-window.component';
 import { ProhibitedAppsComponent } from './prohibited-apps/prohibited-apps.component';
+import { LogoutNowComponent } from './logout-now/logout-now.component';
 
 const routes: Routes = [
   { path: 'user-time', component: UserTimeComponent },
   { path: 'time-window', component: TimeWindowComponent },
   { path: 'prohibited-apps', component: ProhibitedAppsComponent },
+  { path: 'logout-now', component: LogoutNowComponent },
   // Add more routes as needed
 ];
 
